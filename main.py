c01ea466055a15653324955498425e9dc5d0f6ed
wbswjc <me@wbswjc.com>
2019-10-12 16:38:03 +0800

    Dump data

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
+import shutil
+
 import pymysql
+import yaml
 
 
 class ConnectionError(Exception):
@@ -33,8 +37,8 @@ def conn():
                            cursorclass=pymysql.cursors.DictCursor)
 
 
-# tables returns list of table names in a database
-def tables(conn):
+# get_table_names returns list of table names in a database
+def get_table_names(conn):
     tables = []
     with conn.cursor() as cursor:
         sql = "show tables"
@@ -45,16 +49,16 @@ def tables(conn):
     return tables
 
 
-# create_table returns create table statement of a table
-def create_table(conn, table_name):
+# get_create_table_stmt returns create table statement of a table
+def get_create_table_stmt(conn, table_name):
     with conn.cursor() as cursor:
         cursor.execute("show create table {}".format(table_name))
         result = cursor.fetchone()
     return result["Create Table"]
 
 
-# columns returns list of columns in a table
-def columns(conn, table_name):
+# get_columns returns list of columns in a table
+def get_columns(conn, table_name):
     columns = []
     with conn.cursor() as cursor:
         cursor.execute("desc {}".format(table_name))
@@ -63,14 +67,68 @@ def columns(conn, table_name):
     return columns
 
 
-try:
-    c = conn()
+# get_rows returns list of rows in a table
+def get_rows(conn, table_name):
+    rows = []
+    with conn.cursor() as cursor:
+        cursor.execute("select * from {}".format(table_name))
+        for row in cursor.fetchall():
+            rows.append(row)
+    return rows
+
+
+def dump_table(c, table_dir, table_name):
+    print("Processing table {}:".format(table_name))
+    os.mkdir(table_dir)
+    with open(os.path.join(table_dir, "create_table.sql"), "w") as f:
+        f.write(get_create_table_stmt(c, table_name))
+    with open(os.path.join(table_dir, "desc_table.yaml"), "w") as f:
+        columns = get_columns(c, table_name)
+        f.write(yaml.dump(columns))
+    # os.mkdir(os.path.join(table_dir, "rows"))
+    for column in columns:
+        if column["Type"] in ["text", "mediumtext", "longtext"]:
+            os.mkdir(os.path.join(table_dir,
+                                  "column_{}".format(column["Field"])))
+    rows = ""
+    for i, row in enumerate(get_rows(c, table_name)):
+        text_list = []
+        for column in columns:
+            if column["Type"] in ["text", "mediumtext", "longtext"]:
+                with open(os.path.join(table_dir,
+                                       "column_{}".format(column["Field"]),
+                                       "{}.txt".format(i)), "w") as c:
+                    c.write(str(row[column["Field"]]))
+                text_list.append("_")
+            else:
+                text_list.append(str(row[column["Field"]]) \
+                                 .replace("\\", "\\\\") \
+                                 .replace(",", "\\,") \
+                                 .replace("\n", "\\n"))
+        rows += ",".join(text_list) + "\n"
+    with open(os.path.join(table_dir, "rows.txt"), "w") as f:
+        f.write(rows)
+
+
+def dump():
+    dataDir = "data"
+    if os.path.isdir(dataDir):
+        shutil.rmtree(dataDir)
+    elif os.path.isfile(dataDir):
+        os.remove(dataDir)
+    os.mkdir(dataDir)
+
     try:
-        for table in tables(c):
-            print("Processing table {}:".format(table))
-            print(create_table(c, table), "\n\n", columns(c, table), "\n\n")
-    finally:
-        c.close()
-except ConnectionError as e:
-    print(e)
-    exit(1)
+        c = conn()
+        try:
+            for table in get_table_names(c):
+                dump_table(c, os.path.join(dataDir, table), table)
+        finally:
+            c.close()
+    except ConnectionError as e:
+        print(e)
+        exit(1)
+
+
+if __name__ == '__main__':
+    dump()
