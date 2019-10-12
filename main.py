f3a90d0c5b9dc2a17913ba26995ad3d80a19b1e5
wbswjc <me@wbswjc.com>
2019-10-13 00:21:07 +0800

    Show process

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ import click
 import pymysql
 import yaml
 
+VERBOSE_NONE = 0
+
+VERBOSE_IMPORTANT = 1
+
+VERBOSE_EVERYTHING = 2
+
 
 class ConnectionError(Exception):
     pass
@@ -97,21 +103,34 @@ def drop_table_if_exists(conn, table_name):
             raise e
 
 
-def dump_table(conn, table_dir, table_name):
-    print("Processing table {}:".format(table_name))
+def dump_table(conn, table_dir, table_name, verbose=VERBOSE_NONE):
+    print('Processing table "{}"...'.format(table_name))
     os.mkdir(table_dir)
     with open(os.path.join(table_dir, "create_table.sql"), "w") as f:
+        if verbose > VERBOSE_NONE:
+            print("  Saving create table statement...")
         f.write(get_create_table_stmt(conn, table_name))
     with open(os.path.join(table_dir, "desc_table.yaml"), "w") as f:
+        if verbose > VERBOSE_NONE:
+            print("  Saving desc table output...")
         columns = get_columns(conn, table_name)
         f.write(yaml.dump(columns))
-    # os.mkdir(os.path.join(table_dir, "rows"))
+    if verbose > VERBOSE_NONE:
+        print("  Checking columns...")
     for column in columns:
         if column["Type"] in ["text", "mediumtext", "longtext"]:
+            if verbose > VERBOSE_NONE:
+                click.echo(click.style(
+                    '  Column "{}" is long data.'.format(column["Field"]),
+                    fg='yellow'))
             os.mkdir(os.path.join(table_dir,
                                   "column_{}".format(column["Field"])))
     rows = ""
     for i, row in enumerate(get_rows(conn, table_name)):
+        if verbose > VERBOSE_IMPORTANT:
+            print('  -- {}'.format(i))
+        elif verbose > VERBOSE_NONE:
+            print('.', end='')
         text_list = []
         for column in columns:
             if column["Type"] in ["text", "mediumtext", "longtext"]:
@@ -125,22 +144,28 @@ def dump_table(conn, table_dir, table_name):
         rows += json.dumps(text_list) + "\n"
     with open(os.path.join(table_dir, "rows.txt"), "w") as f:
         f.write(rows)
+    print()
 
 
-def dump(data_dir, verbose=0):
+def dump(data_dir, verbose=VERBOSE_NONE):
     if os.path.isdir(data_dir):
+        print('"{}" is a directory, removing...'.format(data_dir))
         shutil.rmtree(data_dir)
     elif os.path.isfile(data_dir):
+        print('"{}" is a file, removing...'.format(data_dir))
         os.remove(data_dir)
+    print('Creating directory "{}"...'.format(data_dir))
     os.mkdir(data_dir)
 
     try:
         conn = connection()
         try:
             for table in get_table_names(conn):
-                dump_table(conn, os.path.join(data_dir, table), table)
+                dump_table(conn, os.path.join(data_dir, table), table,
+                           verbose=verbose)
         finally:
             conn.close()
+        print("Done!")
     except ConnectionError as e:
         print(e)
         exit(1)
@@ -188,7 +213,7 @@ def recover_table(conn, table_dir, table_name):
             lineNumber += 1
 
 
-def recover(data_dir, verbose=0):
+def recover(data_dir, verbose=VERBOSE_NONE):
     if not os.path.isdir(data_dir):
         raise RecoverError("\"{}\" is not a directory".format(data_dir))
 
@@ -206,7 +231,7 @@ def recover(data_dir, verbose=0):
 
 @click.command()
 @click.argument('action')
-@click.option('--verbose', default=0, help='Verbose level.')
+@click.option('--verbose', default=VERBOSE_NONE, help='Verbose level.')
 def main(verbose, action):
     if action == "dump":
         dump("data", verbose=verbose)
